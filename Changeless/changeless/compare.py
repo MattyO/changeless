5ee25435f90685330940c6f6dade4a65d89c6bd4
Matt ODonnell <matt.odonnell@leandog.com>
2013-07-25 14:08:51 -0400

    change some naming forgot to include in last commit

--- Changeless/changeless/compare.py
+++ Changeless/changeless/compare.py
@@ -1,12 +1,13 @@
+from changeless.methods import to_dict, _r_sub_dict
 '''returns true if the union of dicts are the same minus the key in the ignore list'''
 def fuzzyEquals(obj, other_obj, ignore=[]):
-    obj = convert_immutables(obj)
-    other_obj =convert_immutables(other_obj)
+    obj = to_dict(obj)
+    other_obj =to_dict(other_obj)
 
     refined_obj = _r_sub_dict(other_obj, obj)
     refined_other = _r_sub_dict(obj, other_obj )
 
     if refined_obj != refined_other:
-            pass
+        pass
 
     return refined_obj == refined_other 
