86326559f0ef693956cb73754e419ef6a38f5c5b
Matt ODonnell <matt.odonnell@leandog.com>
2013-08-10 18:21:07 -0400

    all decorators now take depth

--- Changeless/changeless/decorators.py
+++ Changeless/changeless/decorators.py
@@ -1,32 +1,75 @@
 from changeless.types import FancyModel, ImmutableModel
 
-def fancy_list(db_function):
-    def create_fancy():
-        return lambda: [ FancyModel(db_entry) for db_entry in db_function() ]
-    return create_fancy()
+def fancy_list(*args, **kwargs):
+    depth = 1
+    if kwargs.has_key('depth'):
+        depth = kwargs['depth']
 
-def immutable_list(db_function):
-    def create_immutable():
-        return lambda: [ ImmutableModel(db_entry) for db_entry in db_function() ]
-    return create_immutable()
+    def wrapped(db_function):
+        def create_fancy():
+            print "depth is " + str(depth)
+            return lambda: [ FancyModel(db_entry, depth) for db_entry in db_function() ]
+        return create_fancy()
 
-def fancy_gen(db_function):
-    def create_fancy():
-        return lambda: _fancy_gen( db_function() )
+    if len(args) == 1 and callable(args[0]):
+        return wrapped(args[0])
+    else:
+        return wrapped
 
-    return create_fancy()
+def immutable_list(*args, **kwargs):
+    depth = 1
+    if kwargs.has_key('depth'):
+        depth = kwargs['depth']
 
-def immutable_gen(db_function):
-    def create_immutable():
-        return lambda: _immutable_gen( db_function() )
+    def wrapped(db_function):
+        def create_immutable():
+            return lambda: [ ImmutableModel(db_entry, depth) for db_entry in db_function() ]
 
-    return create_immutable()
+        return create_immutable()
 
-def _immutable_gen(query_set):
+    if len(args) == 1 and callable(args[0]):
+        return wrapped(args[0])
+    else:
+        return wrapped
+
+def fancy_gen(*args, **kwargs):
+    depth = 1
+    if kwargs.has_key('depth'):
+        depth = kwargs['depth']
+
+    def wrapped(db_function):
+
+        def create_fancy():
+            return lambda: _fancy_gen( db_function(), depth )
+        return create_fancy()
+
+    if len(args) == 1 and callable(args[0]):
+        return wrapped(args[0])
+    else:
+        return wrapped
+
+
+def immutable_gen(*args, **kwargs):
+    depth = 1
+    if kwargs.has_key('depth'):
+        depth = kwargs['depth']
+
+    def wrapped(db_function):
+        def create_immutable():
+            return lambda: _immutable_gen( db_function(), depth )
+
+        return create_immutable()
+
+    if len(args) == 1 and callable(args[0]):
+        return wrapped(args[0])
+    else:
+        return wrapped
+
+def _immutable_gen(query_set, depth):
     for db_entry in query_set:
-        yield FancyModel(db_entry)
+        yield FancyModel(db_entry, depth)
 
-def _fancy_gen(query_set):
+def _fancy_gen(query_set, depth):
     for db_entry in query_set:
-        yield FancyModel(db_entry)
+        yield FancyModel(db_entry, depth)
 
--- Changeless/changeless/test/test_decorators.py
+++ Changeless/changeless/test/test_decorators.py
@@ -1,9 +1,10 @@
-from changeless.decorators import fancy_list, immutable_list
+from changeless.decorators import fancy_list, immutable_list, fancy_gen, immutable_gen 
 from changeless.types import ImmutableHash, ImmutableModel, FancyHash, FancyModel
 
 
 import unittest
 import os
+import types
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
@@ -57,4 +58,103 @@ class TestDecorators(unittest.TestCase):
         immutable_book_list = get_cities()
 
         self.assertEqual(immutable_book_list[0].title , "A Tale of Two Cities")
+
+    def test_fancy_list_should_be_able_to_submit_depth(self):
+        @fancy_list(depth=1)
+        def get_cities():
+            return Book.objects.filter(title="A Tale of Two Cities")
+
+        fancy_book_list = get_cities()
+        self.assertEqual(fancy_book_list[0].title , "A Tale of Two Cities")
+
+    def test_fancy_list_depth_attribte_should_effect_depth(self):
+        @fancy_list(depth=0)
+        def get_cities():
+            return Book.objects.filter(title="A Tale of Two Cities")
+
+        fancy_book_list = get_cities()
+
+        with self.assertRaises(AttributeError):
+            fancy_book_list[0].readers
+
+
+    def test_immutable_list_should_be_able_to_submit_depth(self):
+        @immutable_list(depth=1)
+        def get_cities():
+            return Book.objects.filter(title="A Tale of Two Cities")
+
+        immutable_book_list = get_cities()
+        self.assertEqual(immutable_book_list[0].title , "A Tale of Two Cities")
+
+    def test_immutable_list_depth_attrubte_should_effect_depth(self):
+
+        @immutable_list(depth=0)
+        def get_cities():
+            return Book.objects.filter(title="A Tale of Two Cities")
+
+        immutable_book_list = get_cities()
+
+        with self.assertRaises(AttributeError):
+            immutable_book_list[0].readers
+
+    def test_fancy_gen_should_return_a_generator(self):
+        @fancy_gen
+        def get_cities():
+            return Book.objects.filter(title="A Tale of Two Cities")
+
+        fancy_book_list = get_cities()
+        self.assertIsInstance(fancy_book_list, types.GeneratorType)
+
+
+    def test_fancy_gen_should_be_able_to_submit_depth(self):
+        @fancy_gen(depth=1)
+        def get_cities():
+            return Book.objects.filter(title="A Tale of Two Cities")
+
+        fancy_book_generator = get_cities()
+        fancy_book_list = [ book for book in fancy_book_generator ]
+
+        self.assertEqual(fancy_book_list[0].title , "A Tale of Two Cities")
+
+    def test_fancy_gen_depth_attrubte_should_effect_depth(self):
+
+        @fancy_gen(depth=0)
+        def get_cities():
+            return Book.objects.filter(title="A Tale of Two Cities")
+
+        fancy_book_generator = get_cities()
+        fancy_book_list = [ book for book in fancy_book_generator ]
+
+        with self.assertRaises(AttributeError):
+            fancy_book_list[0].readers
+
+    def test_immutable_gen_should_return_a_generator(self):
+        @immutable_gen
+        def get_cities():
+            return Book.objects.filter(title="A Tale of Two Cities")
+
+        immutable_book_generator = get_cities()
+        self.assertIsInstance(immutable_book_generator, types.GeneratorType)
+
+    def test_immutable_gen_should_be_able_to_submit_depth(self):
+        @immutable_gen(depth=1)
+        def get_cities():
+            return Book.objects.filter(title="A Tale of Two Cities")
+
+        fancy_book_generator = get_cities()
+        fancy_book_list = [ book for book in fancy_book_generator ]
+
+        self.assertEqual(fancy_book_list[0].title , "A Tale of Two Cities")
+
+    def test_immutable_gen_depth_attrubte_should_effect_depth(self):
+
+        @immutable_gen(depth=0)
+        def get_cities():
+            return Book.objects.filter(title="A Tale of Two Cities")
+
+        fancy_book_generator = get_cities()
+        fancy_book_list = [ book for book in fancy_book_generator ]
+
+        with self.assertRaises(AttributeError):
+            fancy_book_list[0].readers
 
