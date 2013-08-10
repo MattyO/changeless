acc133bbd6aa09b96172dea0b1da48ecebea3968
Matt ODonnell <matt.odonnell@leandog.com>
2013-08-10 15:15:58 -0400

    removed intermedeary function to create object in decorators

--- Changeless/changeless/decorators.py
+++ Changeless/changeless/decorators.py
@@ -1,13 +1,13 @@
-from changeless.types import create_fancy_model, create_immutable_model
+from changeless.types import FancyModel, ImmutableModel
 
 def fancy_list(db_function):
     def create_fancy():
-        return lambda: [ create_fancy_model(db_entry) for db_entry in db_function() ]
+        return lambda: [ FancyModel(db_entry) for db_entry in db_function() ]
     return create_fancy()
 
 def immutable_list(db_function):
     def create_immutable():
-        return lambda: [ create_immutable_model(db_entry) for db_entry in db_function() ]
+        return lambda: [ ImmutableModel(db_entry) for db_entry in db_function() ]
     return create_immutable()
 
 def fancy_gen(db_function):
@@ -24,9 +24,9 @@ def immutable_gen(db_function):
 
 def _immutable_gen(query_set):
     for db_entry in query_set:
-        yield create_immutable_model(db_entry)
+        yield FancyModel(db_entry)
 
 def _fancy_gen(query_set):
     for db_entry in query_set:
-        yield create_fancy_model(db_entry)
+        yield FancyModel(db_entry)
 
