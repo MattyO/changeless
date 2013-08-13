d6f11857d549b199c007ac15a42b7cf282a6c6b0
Matt ODonnell <matt.odonnell@leandog.com>
2013-08-13 14:54:39 -0400

    nose test file log name changed

--- Changeless/run_nose.py
+++ Changeless/run_nose.py
@@ -2,11 +2,11 @@ import os
 import nose
 import logging
 
-logging.basicConfig(filename="fileinfo", level=logging.DEBUG)
+logging.basicConfig(filename="test_log.txt", level=logging.DEBUG)
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
-print __file__
+
 os.chdir(os.path.abspath(os.path.dirname(__file__)))
 
 nose.main()
